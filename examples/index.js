2e9f77550826e57306c4ecdadc7dec529062754b
Alan Hortz <alan.hortz@gmail.com>
2016-08-08 22:00:06 +0200

    attempt to run a child process to handle connection / disconnection

--- examples/index.js
+++ examples/index.js
@@ -1,14 +1,14 @@
-const spawn = require('child_process').spawn;
+var spawn = require('child_process').spawn;
 
 
 function launchChild() {
     var ls = spawn('node', ['accelerometer.js']);
     
-    ls.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
+    ls.on('close', function(code) {
+        console.log('child process exited with code ' + code);
         launchChild();
     });
-};
+}
 
 
 launchChild();
