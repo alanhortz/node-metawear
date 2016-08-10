c6837ff95db09a2b62277c80b67ac009791b893c
Alan Hortz <alan.hortz@gmail.com>
2016-08-10 15:44:00 +0200

    last message

--- examples/accelerometer.js
+++ examples/accelerometer.js
@@ -7,20 +7,60 @@ var range = parseFloat(process.argv[3]) || 2;
 var sensor = null;
 var id = 'c9ee6389a176';
 
-devices.discoverById(id, function(device) {
-    
-    sensor = device;
+// TODO : Retest the discovery with device.discoverById(id, callback)
+
+devices.discover(function(device) {
+
+    console.log('discovered device ', device.address, device.uuid);
 
     device.connectAndSetup(function() {
-        console.log('connected and setup');
+        
+        console.log('were connected!');
+        console.log('Starting accelerometer with ' + rate + 'hz ang +-' + range + 'g');
+        
+        var accelerometer = new device.Accelerometer(device);
+        var logger        = new device.Log(device);
+        var switched = true;
+
+        accelerometer.setOutputDataRate(rate);
+        accelerometer.setAxisSamplingRange(range);
+        logger.startLogging(false);
+
+        accelerometer.setConfig();
+        accelerometer.enableNotifications();
+        accelerometer.enableAxisSampling();
+        accelerometer.start();
+
+        accelerometer.onChange(function(data) {
+            console.log("x:", data.x, "\t\ty:", data.y, "\t\tz:", data.z);
+        });
+
+        setInterval(function() {
+            if(device.connectedAndSetUp) {
+                if(switched) {
+                    accelerometer.stop();
+                    switched = false;
+                    device._peripheral.updateRssi();
+                } else {
+                    accelerometer.start();
+                    switched = true;
+                }
+            }
+        }, 1000);
+
+
         setTimeout(function() {device.disconnect();}, 2000);
     });
-
 
     device.on('disconnect', function() {
         console.log('we got disconnected! :( ');
     });
 
+    device._peripheral.on('rssiUpdate', function (rssi){
+       console.log(rssi);
+       if(rssi < -75) device.disconnect();
+    });
+    
 });
 
 // var startDiscovering = function(id) {
