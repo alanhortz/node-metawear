861750eead56761ba4ddf89d5578f8a6899e4e86
Alan Hortz <alan.hortz@gmail.com>
2016-08-12 15:52:29 +0200

    refining the location based on kalman filter

--- examples/accelerometer.js
+++ examples/accelerometer.js
@@ -1,4 +1,5 @@
 
+require("javascript.util");
 var devices = require('./../src/device');
 
 var rate  = parseFloat(process.argv[2]) || 50;
@@ -7,6 +8,19 @@ var range = parseFloat(process.argv[3]) || 2;
 var sensor = null;
 var id = 'c9ee6389a176';
 
+
+var ArrayList = javascript.util.ArrayList;
+var Arrays = javascript.util.Arrays;
+
+
+var lastError = 1;
+var distanceBuffor  = new ArrayList();
+var medianBuffor = new ArrayList();
+var kalmanEstimation = 0;
+var bufforSize = 6;
+
+var lowerThreshold = 0.4;
+var higherThreshold = 0.9;
 
 // TODO : Retest the discovery with device.discoverById(id, callback)
 
@@ -52,19 +66,6 @@ devices.discover(function(device) {
             }
         }, 1000);
 
-        led.config
-            .setColor(led.config.BLUE)
-            .setRiseTime(1000)
-            .setHighTime(500)
-            .setFallTime(1000)
-            .setPulseDuration(2500)
-            .setRepeatCount(20)
-            .setHighIntensity(16)
-            .setLowIntensity(1);
-
-        led.commitConfig();
-        led.play(true);
-
 
     });
 
@@ -76,110 +77,135 @@ devices.discover(function(device) {
 
     device._peripheral.on('rssiUpdate', function (rssi){
        console.log(rssi);
-
-       if(-90 > rssi > -70) {
-            led.config
-                .setColor(led.config.RED)
-                .setRiseTime(1000)
-                .setHighTime(500)
-                .setFallTime(1000)
-                .setPulseDuration(2500)
-                .setRepeatCount(20)
-                .setHighIntensity(16)
-                .setLowIntensity(1);
-
-            led.commitConfig();
-            led.play(true);        
-       }
-
-       if(-70 > rssi > -50) {
-            led.config
-                .setColor(led.config.BLUE)
-                .setRiseTime(1000)
-                .setHighTime(500)
-                .setFallTime(1000)
-                .setPulseDuration(2500)
-                .setRepeatCount(20)
-                .setHighIntensity(16)
-                .setLowIntensity(1);
-
-            led.commitConfig();
-            led.play(true);        
-       }
-
-       if(-50 > rssi > -20) {
-            led.config
-                .setColor(led.config.GREEN)
-                .setRiseTime(1000)
-                .setHighTime(500)
-                .setFallTime(1000)
-                .setPulseDuration(2500)
-                .setRepeatCount(20)
-                .setHighIntensity(16)
-                .setLowIntensity(1);
-
-            led.commitConfig();
-            led.play(true);        
-       }
-
+       getProximity(-59, rssi);
 
        if(rssi < -75) device.disconnect();
     });
     
 });
 
-// var startDiscovering = function(id) {
-// devices.discoverById(id, function(device) {
-// //devices.discover(function(device) {
-//     console.log('discovered device ', device.address, device.uuid);
-
-//     device.on('disconnect', function() {
-//         console.log('we got disconnected! :( ');
-//         //startDiscovering(id);
-//     });
-
-//     device.connectAndSetup(function(error) {
-//         console.log('were connected!');
-
+function calculateAccuracy(measuredPower, rssi) {
   
-//       console.log('Start accelerometer with ' + rate + 'hz ang +-' + range + 'g');
+  //var txPower = -59 //hard coded power value. Usually ranges between -59 to -65
+  
+  return Math.pow(12.0, 1.5 * ((rssi / measuredPower) - 1));
 
-//         var accelerometer = new device.Accelerometer(device);
-//         var logger        = new device.Log(device);
-// 	var switched = true;
+  // if (rssi == 0) {
+  //   return -1.0; 
+  // }
+ 
+  // var ratio = rssi*1.0/txPower;
+  // if (ratio < 1.0) {
+  //   return Math.pow(ratio,10);
+  // }
+  // else {
+  //   var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;    
+  //   return distance;
+  // }
+}
 
-//         accelerometer.setOutputDataRate(rate);
-//         accelerometer.setAxisSamplingRange(range);
-//         logger.startLogging(false);
+function calculateProximity(accuracy) {
 
-//         accelerometer.setConfig();
-//         accelerometer.enableNotifications();
-//         accelerometer.enableAxisSampling();
-//         accelerometer.start();
+  var proximity;
 
-//         accelerometer.onChange(function(data) {
-//             console.log("x:", data.x, "\t\ty:", data.y, "\t\tz:", data.z);
-//         });
+      if (accuracy < 0) {
+        proximity = 'unknown';
+      } else if (accuracy < 0.5) {
+        proximity = 'immediate';
+      } else if (accuracy < 4.0) {
+        proximity = 'near';
+      } else {
+        proximity = 'far';
+      }
+  
+  return proximity;
 
-// 	setInterval(function() {
-// 	    if(device.connectedAndSetUp) {
-// 	    	if(switched) {
-//               	accelerometer.stop();
-// 	      	switched = false;
-// 	      	device._peripheral.updateRssi();	      
-// 	    	} else {
-// 	      	accelerometer.start();
-// 	      	switched = true;
-// 	    	}
-// 	    }
-// 	}, 1000);
+}
 
-//     });
+function getMedian(arrayList) {
+    var middle = arrayList.size()/2;
+    if (arrayList.size() % 2 == 1) {
+        return arrayList.get(middle);
+    } else {
+        return (arrayList.get(middle-1) + arrayList.get(middle)) / 2.0;
+    }
+}
 
-//     device._peripheral.on('rssiUpdate', function (rssi){
-//        console.log(rssi);
-//        if(rssi < -75) device.disconnect();
-//     });
-// });
-// };
-// startDiscovering('c9ee6389a176');
+function getProximity(measuredPower, rssi) {
+        var result;
+
+        var varianceSum = 0;
+        var expectedDistance = 0;
+        var tempDistance = calculateAccuracy(measuredPower, rssi);
+        var proximity = calculateProximity(tempDistance);
+
+        
+        if (distanceBuffor.size() == bufforSize) {
+            distanceBuffor.remove(distanceBuffor.get(0));
+            //console.log('clean buffor');
+        }
+        
+        distanceBuffor.add(tempDistance);
+
+        if (proximity == 'immediate') {
+            expectedDistance = 0.8625;
+            lastError = 1;
+            kalmanEstimation = 0;
+        } else if (proximity == 'near') {
+            expectedDistance = 1.75;
+            lastError = 1;
+            kalmanEstimation = 0;
+        } else if (proximity == 'far') {
+            expectedDistance = 2.625;
+            lastError = 1;
+            kalmanEstimation = 0;
+        }
+
+        for (var k = 0; k < distanceBuffor.size(); k++) {
+            varianceSum = varianceSum + (distanceBuffor.get(k) - expectedDistance) * (distanceBuffor.get(k) - expectedDistance);
+        }
+
+        var variance = varianceSum / bufforSize;
+        var standardDeviation = Math.sqrt(variance);
+
+        var kalmanParameter = lastError / (lastError + standardDeviation);
+        kalmanEstimation = kalmanEstimation + kalmanParameter * (tempDistance - kalmanEstimation);
+        lastError = (1 - kalmanParameter) * lastError;
+
+        if (medianBuffor.size() >= bufforSize) {
+            medianBuffor.remove(medianBuffor.get(0));
+
+        }
+
+        medianBuffor.add(kalmanEstimation);
+        var medianBufforSorted = new ArrayList();
+        
+        // for(var i = 0; i < medianBufforSorted.size(); i++) {
+        //   medianBufforSorted.remove(i);  
+        // }
+        
+        
+        medianBufforSorted.addAll(medianBuffor);
+        var arMedianBufforSorted = medianBufforSorted.toArray();
+        javascript.util.Arrays.sort(arMedianBufforSorted);
+
+        medianBufforSorted = javascript.util.Arrays.asList(arMedianBufforSorted);
+
+        var median = getMedian(medianBufforSorted);
+
+        
+
+        if ( median < lowerThreshold) {
+          result = 'immediate';
+        } else if ( median < higherThreshold) {
+          result = 'near';
+        } else if ( median >= higherThreshold) {
+          result = 'far';
+        }
+
+        if (result) {
+          console.log('name : ', name, ' result: ', result);
+        }
+
+        return result;
+}
